7d059c7cba25fcbdac2cdf12c32115521e66843c
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2023-11-03 10:02:57 +0300

    Add better comments

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,10 +2,14 @@ import Ship from "./ship";
 
 export default class Gameboard {
   constructor() {
+    // Initialize the gameboard grid as a 10x10 grid with all cells initially set to null.
     this.grid = this.createGrid(10, 10, null);
+
+    // Initialize an array to track missed attacks.
     this.missedAttacks = [];
   }
   
+  // Create a 2D grid with the specified number of rows and columns, initialized with a given value.
   createGrid(rows, cols, initialValue) {
     const grid = [];
     for (let i = 0; i < rows; i++) {
@@ -18,14 +22,18 @@ export default class Gameboard {
     return grid;
   }
 
+  // Check if a cell at given coordinates (y, x) is empty (null).
   emptyCellGrid(y, x) {
     return this.grid[y][x] === null;
   }
+
+  // Place a ship on the gameboard at specified coordinates (y, x) and with a given orientation.
   placeShip(ship, y, x, orientation) {
     const coordinatesToCheck = [];
 
     switch (orientation) {
       case 'horizontal':
+        // Check if the ship would go out of bounds horizontally.
         if (x + ship.length > 10) {
           throw new Error(`Ship placement would go out of bounds horizontally: it takes ${ship.length} spaces.`);
         }
@@ -34,6 +42,7 @@ export default class Gameboard {
         }
         break;
       case 'vertical':
+        // Check if the ship would go out of bounds vertically.
         if (y + ship.length > 10) {
           throw new Error(`Ship placement would go out of bounds vertically: it takes ${ship.length} spaces.`);
         }
@@ -43,7 +52,7 @@ export default class Gameboard {
         break;
     }
 
-    // Check all coordinates before placing the ship
+    // Check if all specified coordinates are empty before placing the ship.
     if (coordinatesToCheck.every(([cy, cx]) => this.emptyCellGrid(cy, cx))) {
       coordinatesToCheck.forEach(([cy, cx]) => {
         this.grid[cy][cx] = ship;
@@ -53,25 +62,32 @@ export default class Gameboard {
     }
   }
 
+  // Process an attack at specified coordinates (y, x) and record hits or misses.
   receiveAttack(y, x) {
-    // If the coordinates are not occupied
+    // If the coordinates are not occupied by a ship.
     if (this.grid[y][x] === null) {
+      // Record a miss and add the attack coordinates to the missedAttacks array.
       this.grid[y][x] = 'miss';
-      this.missedAttacks.push({x , y});
+      this.missedAttacks.push({ x, y });
     } else {
-      // Store the ship in a variable and register the hit
+      // If a ship occupies the cell, record a hit.
       const ship = this.grid[y][x];
       ship.hit(1);
       this.grid[y][x] = 'hit';
     }
   }
 
+  // Get the array of missed attack coordinates.
   getMissedAttacks() {
-    return this.missedAttacks
+    return this.missedAttacks;
   }
 
+  // Check if all ships on the gameboard have been sunk, indicating a win condition.
   allShipsSunk() {
-    const allShips = this.grid.flat().filter(coordiante => coordiante instanceof Ship)
+    // Flatten the grid to get a single array of all grid cells containing ships.
+    const allShips = this.grid.flat().filter(coordinate => coordinate instanceof Ship);
+    
+    // Check if every ship in the array is sunk, returning true for all ships sunk.
     return allShips.every(ship => ship.isSunk());
   }
 }
